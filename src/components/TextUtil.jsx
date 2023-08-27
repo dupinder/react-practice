04866066b9911d04669f88ad3541b420568e7f24
Dupinder <dupinderdhiman5@gmail.com>
2023-08-28 00:01:38 +0530

    feat: add new :guitar: to the text util
    # find no of characters
    # find no of words
    # Toggle capitalize words

--- src/components/TextUtil.jsx
+++ src/components/TextUtil.jsx
@@ -1,17 +1,47 @@
 import React from "react";
+import {useState} from 'react';
 
 export default function TextUtil() {
+
+  const [text, setText] = useState("Enter text here");
+
+  const capitalizeText = () => {
+    setText(text.toUpperCase());
+  }
+  
+  const unCapitalizeText = () => {
+    setText(text.toLowerCase());
+  }
+
+  const changeText = (e) => {
+    setText(e.target.value);
+  }
+  
   return (
     <>
-      <div className="form-floating" >
-        <textarea
-          className="form-control"
-          placeholder="Leave a comment here"
-          id="floatingTextarea2"
-          style={{height: '100px', width: '70%', margin: '50px'}}
-        ></textarea>
-        <label style={{height: '100px', width: '70%', margin: '50px'}} htmlFor="floatingTextarea2">Add text to me ✍️</label>
-      </div>
+      <div className="form-floating container" >
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            style={{height: '100px', width: '70%', margin: '50px'}}
+            onChange={changeText}
+            value={text}
+          ></textarea>
+          <label style={{height: '100px', width: '70%', margin: '50px'}} htmlFor="floatingTextarea2">Add text to me ✍️</label>
+        </div>
+        <div className="container my-2">
+          <div className="mx-2"><button className="btn btn-primary" onClick={capitalizeText}>Capitalize</button></div>
+          <div className="mx-2"><button className="btn btn-primary" onClick={unCapitalizeText}>Un-Capitalize</button></div>
+        </div>
+
+        <div className="container my-2">
+          <h1>Text Info</h1>
+          <div className="mx-2">
+            <span className="mx-2"><h5>Word Count: {text.split(' ').length}</h5></span>
+            <span className="mx-2"><h5>Charactor Count: {text.length}</h5></span>
+          </div>
+        </div>
     </>
   );
 }
